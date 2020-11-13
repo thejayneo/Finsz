fc37911526f57320970f20eacd08edb5ed37ac35
Jackson <44676403+thejayneo@users.noreply.github.com>
2020-11-13 18:45:40 +1100

    Added user data fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :display_name, :first_name, :last_name, :password, :password_confirmation) }
+        end
+
+        def after_sing_in_path_for(user)
+            stored_location_for(user) || products_path
+        end
 end
