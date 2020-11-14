24f9bffa170959a1bcef9a449a17eddb3723fec3
Jackson <44676403+thejayneo@users.noreply.github.com>
2020-11-15 10:31:32 +1100

    Changed data type for product type. Added user auth timeout.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price, :user, :picture)
+      params.require(:product).permit(:name, :price, :user, :picture, :product_type)
     end
 end
