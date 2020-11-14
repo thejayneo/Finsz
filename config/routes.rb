a1f09fa7e2602854e134419bed8f82e741adeea7
Jackson <44676403+thejayneo@users.noreply.github.com>
2020-11-15 08:59:27 +1100

    Added picture to products model and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   root 'home#index'
-  get '*path', to: 'home#index', via: :all
+  get '*path', to: redirect('/'), constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/user' => "products#index", :as => :user_root
+
 end
