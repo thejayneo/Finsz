610d3f449b6654773833c39cc18469ab1de6bed1
Jackson <44676403+thejayneo@users.noreply.github.com>
2020-11-18 12:43:27 +1100

    Push to production.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,12 +7,21 @@ class PaymentsController < ApplicationController
     def webhook
         payment_id= params[:data][:object][:payment_intent]
         payment = Stripe::PaymentIntent.retrieve(payment_id)
-            product_id = payment.metadata.product_id
-            buyer_id = payment.metadata.buyer_id
-            seller_id = payment.metadata.buyer_id
-            product_name = payment.metadata.product_name
-            product_price = payment.metadata.product_price
+        
+        product_id = payment.metadata.product_id
+        buyer_id = payment.metadata.buyer_id
+        seller_id = payment.metadata.buyer_id
+        product_name = payment.metadata.product_name
+        product_price = payment.metadata.product_price
+        
+        p "*******************************"
+        p product_id, buyer_id, seller_id, product_name, product_price
+        p "*******************************"
+        
+        # order_details = JSON.parse(payment.metada)
+        
 
-        Order.create(product_id:product_id, buyer_id:buyer_id, seller_id:seller_id, product_name:product_name, product_price:product_price)
+
+        # Order.create(order_details)
     end
 end
